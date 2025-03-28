a23a36e356d87b3f34144206de73c6a7fdf05b1b
MissterFreeze24 <romainmaufroy3@gmail.com>
2025-03-28 22:01:11 +0100

    Delete US 7 : Création de compte directory

--- US 7 : Création de compte/frontend/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('inscription-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const pseudo = document.getElementById('pseudo').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-
-    // Ici, vous pouvez ajouter la logique pour créer un compte
-    alert('Compte créé pour : ' + pseudo + ' avec l\'email : ' + email);
-});
